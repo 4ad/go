2d514ee8f6859d36fe3828b9c8e33be0978f72e6
Aram Hăvărneanu <aram@mgk.ro>
2015-06-18 14:44:11 +0200

    cmd/internal/obj: reserve sparc64 opcode and register space
    Change-Id: I9cef7d175b9d56c78d94251424580b6d8222f6b9

--- src/cmd/internal/obj/util.go
+++ src/cmd/internal/obj/util.go
@@ -520,11 +520,12 @@ var regSpace []regSet
 const (
 	// Because of masking operations in the encodings, each register
 	// space should start at 0 modulo some power of 2.
-	RBase386   = 1 * 1024
-	RBaseAMD64 = 2 * 1024
-	RBaseARM   = 3 * 1024
-	RBasePPC64 = 4 * 1024 // range [4k, 8k)
-	RBaseARM64 = 8 * 1024 // range [8k, 12k)
+	RBase386     = 1 * 1024
+	RBaseAMD64   = 2 * 1024
+	RBaseARM     = 3 * 1024
+	RBasePPC64   = 4 * 1024 // range [4k, 8k)
+	RBaseARM64   = 8 * 1024 // range [8k, 12k)
+	RBaseSPARC64 = 12 * 1024
 )
 
 // RegisterRegister binds a pretty-printer (Rconv) for register
@@ -584,6 +585,7 @@ const (
 	ABaseAMD64
 	ABasePPC64
 	ABaseARM64
+	ABaseSPARC64
 	AMask = 1<<12 - 1 // AND with this to use the opcode as an array index.
 )
 
