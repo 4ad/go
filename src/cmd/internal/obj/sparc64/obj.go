45d599643897cca0fd354761733a82e4a4b62679
Aram Hăvărneanu <aram@mgk.ro>
2015-08-24 17:46:12 +0300

    cmd/internal/obj/sparc64: find leaf subroutines, strip NOPs
    Change-Id: I8e0495fd5339e07cd39de3a7916922f0b6879e7e

--- src/cmd/internal/obj/sparc64/obj.go
+++ src/cmd/internal/obj/sparc64/obj.go
@@ -16,9 +16,61 @@ func preprocess(ctxt *obj.Link, cursym *obj.LSym) {
 	cursym.Args = cursym.Text.To.Val.(int32)
 	cursym.Locals = int32(cursym.Text.To.Offset)
 
-	for p := cursym.Text.Link; p != nil; p = p.Link {
+	// For future use by oplook and friends.
+	for p := cursym.Text; p != nil; p = p.Link {
 		p.From.Class = aclass(&p.From)
 		p.To.Class = aclass(&p.To)
+	}
+
+	// Find leaf subroutines,
+	// Strip NOPs.
+	var q *obj.Prog
+	var q1 *obj.Prog
+	for p := cursym.Text; p != nil; p = p.Link {
+		switch p.As {
+		case obj.ATEXT:
+			p.Mark |= LEAF
+
+		case obj.ARET:
+			break
+
+		case obj.ANOP:
+			q1 = p.Link
+			q.Link = q1 /* q is non-nop */
+			q1.Mark |= p.Mark
+			continue
+
+		case obj.AJMP, AFBA,
+			obj.ADUFFZERO,
+			obj.ADUFFCOPY:
+			cursym.Text.Mark &^= LEAF
+			fallthrough
+
+		case ABN, ABNE, ABE, ABG, ABLE, ABGE, ABL, ABGU, ABLEU, ABCC, ABCS, ABPOS, ABNEG, ABVC, ABVS,
+			ABRZ, ABRLEZ, ABRLZ, ABRNZ, ABRGZ, ABRGEZ,
+			AFBN, AFBU, AFBG, AFBUG, AFBL, AFBUL, AFBLG, AFBNE, AFBE, AFBUE, AFBGE, AFBUGE, AFBLE, AFBULE, AFBO:
+			q1 = p.Pcond
+
+			if q1 != nil {
+				for q1.As == obj.ANOP {
+					q1 = q1.Link
+					p.Pcond = q1
+				}
+			}
+
+			break
+		}
+
+		q = p
+	}
+
+	for p := cursym.Text; p != nil; p = p.Link {
+		switch p.As {
+		case obj.ATEXT:
+			if cursym.Text.Mark&LEAF != 0 {
+				cursym.Leaf = 1
+			}
+		}
 	}
 }
 
