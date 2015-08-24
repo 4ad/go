6ffe385b271e8a6fa921f65b28d90ec0f3278f4a
Aram Hăvărneanu <aram@mgk.ro>
2015-08-24 14:06:21 +0300

    cmd/internal/obj/sparc64: isolate legacy code into its own file
    For easy copyright tracking.
    
    Change-Id: Ica04db9e4f316abe306345d235ddcf62c4688593

--- src/cmd/internal/obj/sparc64/obj.go
+++ src/cmd/internal/obj/sparc64/obj.go
@@ -7,22 +7,11 @@ package sparc64
 import (
 	"cmd/internal/obj"
 	"encoding/binary"
-	"fmt"
 	"log"
 )
 
 // TODO(aram):
 func preprocess(ctxt *obj.Link, cursym *obj.LSym) {}
-
-func follow(ctxt *obj.Link, s *obj.LSym) {
-	ctxt.Cursym = s
-
-	firstp := ctxt.NewProg()
-	lastp := firstp
-	xfol(ctxt, s.Text, &lastp)
-	lastp.Link = nil
-	s.Text = firstp.Link
-}
 
 func relinv(a int) int {
 	switch a {
@@ -62,134 +51,6 @@ func relinv(a int) int {
 
 	log.Fatalf("unknown relation: %s", Anames[a])
 	return 0
-}
-
-func xfol(ctxt *obj.Link, p *obj.Prog, last **obj.Prog) {
-	var q *obj.Prog
-	var r *obj.Prog
-	var a int
-	var i int
-
-loop:
-	if p == nil {
-		return
-	}
-	a = int(p.As)
-	if a == obj.AJMP {
-		q = p.Pcond
-		if q != nil {
-			p.Mark |= FOLL
-			p = q
-			if !(p.Mark&FOLL != 0) {
-				goto loop
-			}
-		}
-	}
-
-	if p.Mark&FOLL != 0 {
-		i = 0
-		q = p
-		for ; i < 4; i, q = i+1, q.Link {
-			if q == *last || q == nil {
-				break
-			}
-			a = int(q.As)
-			if a == obj.ANOP {
-				i--
-				continue
-			}
-
-			if a == obj.AJMP || a == obj.ARET {
-				goto copy
-			}
-			if q.Pcond == nil || (q.Pcond.Mark&FOLL != 0) {
-				continue
-			}
-			if a != ABE && a != ABNE {
-				continue
-			}
-
-		copy:
-			for {
-				r = ctxt.NewProg()
-				*r = *p
-				if !(r.Mark&FOLL != 0) {
-					fmt.Printf("cant happen 1\n")
-				}
-				r.Mark |= FOLL
-				if p != q {
-					p = p.Link
-					(*last).Link = r
-					*last = r
-					continue
-				}
-
-				(*last).Link = r
-				*last = r
-				if a == obj.AJMP || a == obj.ARET {
-					return
-				}
-				if a == ABNE {
-					r.As = ABE
-				} else {
-					r.As = ABNE
-				}
-				r.Pcond = p.Link
-				r.Link = p.Pcond
-				if !(r.Link.Mark&FOLL != 0) {
-					xfol(ctxt, r.Link, last)
-				}
-				if !(r.Pcond.Mark&FOLL != 0) {
-					fmt.Printf("cant happen 2\n")
-				}
-				return
-			}
-		}
-
-		a = obj.AJMP
-		q = ctxt.NewProg()
-		q.As = int16(a)
-		q.Lineno = p.Lineno
-		q.To.Type = obj.TYPE_BRANCH
-		q.To.Offset = p.Pc
-		q.Pcond = p
-		p = q
-	}
-
-	p.Mark |= FOLL
-	(*last).Link = p
-	*last = p
-	if a == obj.AJMP || a == obj.ARET {
-		return
-	}
-	if p.Pcond != nil {
-		if a != ABL && p.Link != nil {
-			q = obj.Brchain(ctxt, p.Link)
-			if a != obj.ATEXT {
-				if q != nil && (q.Mark&FOLL != 0) {
-					p.As = int16(relinv(a))
-					p.Link = p.Pcond
-					p.Pcond = q
-				}
-			}
-
-			xfol(ctxt, p.Link, last)
-			q = obj.Brchain(ctxt, p.Pcond)
-			if q == nil {
-				q = p.Pcond
-			}
-			if q.Mark&FOLL != 0 {
-				p.Pcond = q
-				return
-			}
-
-			p = q
-			goto loop
-		}
-	}
-
-	p = p.Link
-	goto loop
 }
 
 var unaryDst = map[int]bool{
