27784ba9b109af4d2bfbe34acc4cf14f3e50a43c
Aram Hăvărneanu <aram@mgk.ro>
2015-10-21 16:33:13 +0200

    alt: do all integer conversions in conv.c
    Change-Id: I1863a79749820d71398ddda99c2c395599a45992

--- alt/conv.c
+++ alt/conv.c
@@ -1,15 +1,65 @@
-typedef signed long long int64_t;
-typedef signed int int32_t;
-typedef signed short int int16_t;
+typedef signed long long int64;
+typedef signed int int32;
+typedef signed short int int16;
+typedef signed char int8;
+typedef unsigned long long uint64;
+typedef unsigned int uint32;
+typedef unsigned short int uint16;
+typedef unsigned char uint8;
 
-int16_t
-foo(int64_t x, int64_t y)
-{
-	return (int16_t)y;
-}
-
-int32_t
-bar(int64_t x, int64_t y)
-{
-	return (int32_t)y;
-}
+uint16	uint8_to_uint16(uint8 x)	{ return (uint16)x; }
+uint32	uint8_to_uint32(uint8 x)	{ return (uint32)x; }
+uint64	uint8_to_uint64(uint8 x)	{ return (uint64)x; }
+int8	uint8_to_int8(uint8 x)	{ return (int8)x; }
+int16	uint8_to_int16(uint8 x)	{ return (int16)x; }
+int32	uint8_to_int32(uint8 x)	{ return (int32)x; }
+int64	uint8_to_int64(uint8 x)	{ return (int64)x; }
+uint8	uint16_to_uint8(uint16 x)	{ return (uint8)x; }
+uint32	uint16_to_uint32(uint16 x)	{ return (uint32)x; }
+uint64	uint16_to_uint64(uint16 x)	{ return (uint64)x; }
+int8	uint16_to_int8(uint16 x)	{ return (int8)x; }
+int16	uint16_to_int16(uint16 x)	{ return (int16)x; }
+int32	uint16_to_int32(uint16 x)	{ return (int32)x; }
+int64	uint16_to_int64(uint16 x)	{ return (int64)x; }
+uint8	uint32_to_uint8(uint32 x)	{ return (uint8)x; }
+uint16	uint32_to_uint16(uint32 x)	{ return (uint16)x; }
+uint64	uint32_to_uint64(uint32 x)	{ return (uint64)x; }
+int8	uint32_to_int8(uint32 x)	{ return (int8)x; }
+int16	uint32_to_int16(uint32 x)	{ return (int16)x; }
+int32	uint32_to_int32(uint32 x)	{ return (int32)x; }
+int64	uint32_to_int64(uint32 x)	{ return (int64)x; }
+uint8	uint64_to_uint8(uint64 x)	{ return (uint8)x; }
+uint16	uint64_to_uint16(uint64 x)	{ return (uint16)x; }
+uint32	uint64_to_uint32(uint64 x)	{ return (uint32)x; }
+int8	uint64_to_int8(uint64 x)	{ return (int8)x; }
+int16	uint64_to_int16(uint64 x)	{ return (int16)x; }
+int32	uint64_to_int32(uint64 x)	{ return (int32)x; }
+int64	uint64_to_int64(uint64 x)	{ return (int64)x; }
+uint8	int8_to_uint8(int8 x)	{ return (uint8)x; }
+uint16	int8_to_uint16(int8 x)	{ return (uint16)x; }
+uint32	int8_to_uint32(int8 x)	{ return (uint32)x; }
+uint64	int8_to_uint64(int8 x)	{ return (uint64)x; }
+int16	int8_to_int16(int8 x)	{ return (int16)x; }
+int32	int8_to_int32(int8 x)	{ return (int32)x; }
+int64	int8_to_int64(int8 x)	{ return (int64)x; }
+uint8	int16_to_uint8(int16 x)	{ return (uint8)x; }
+uint16	int16_to_uint16(int16 x)	{ return (uint16)x; }
+uint32	int16_to_uint32(int16 x)	{ return (uint32)x; }
+uint64	int16_to_uint64(int16 x)	{ return (uint64)x; }
+int8	int16_to_int8(int16 x)	{ return (int8)x; }
+int32	int16_to_int32(int16 x)	{ return (int32)x; }
+int64	int16_to_int64(int16 x)	{ return (int64)x; }
+uint8	int32_to_uint8(int32 x)	{ return (uint8)x; }
+uint16	int32_to_uint16(int32 x)	{ return (uint16)x; }
+uint32	int32_to_uint32(int32 x)	{ return (uint32)x; }
+uint64	int32_to_uint64(int32 x)	{ return (uint64)x; }
+int8	int32_to_int8(int32 x)	{ return (int8)x; }
+int16	int32_to_int16(int32 x)	{ return (int16)x; }
+int64	int32_to_int64(int32 x)	{ return (int64)x; }
+uint8	int64_to_uint8(int64 x)	{ return (uint8)x; }
+uint16	int64_to_uint16(int64 x)	{ return (uint16)x; }
+uint32	int64_to_uint32(int64 x)	{ return (uint32)x; }
+uint64	int64_to_uint64(int64 x)	{ return (uint64)x; }
+int8	int64_to_int8(int64 x)	{ return (int8)x; }
+int16	int64_to_int16(int64 x)	{ return (int16)x; }
+int32	int64_to_int32(int64 x)	{ return (int32)x; }
