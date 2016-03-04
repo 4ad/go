53b87755799d6b27527ba0910e7b59cc22423c85
Aram Hăvărneanu <aram@mgk.ro>
2016-03-04 17:53:07 +0100

    cmd/compile/internal/sparc64: generate RNOP correctly

--- src/cmd/compile/internal/sparc64/ggen.go
+++ src/cmd/compile/internal/sparc64/ggen.go
@@ -125,7 +125,7 @@ func appendpp(p *obj.Prog, as int, ftype int, freg int, foffset int64, ttype int
 func ginsnop() {
 	var con gc.Node
 	gc.Nodconst(&con, gc.Types[gc.TINT], 0)
-	gins(sparc64.ARNOP, &con, nil)
+	gins(sparc64.ARNOP, nil, nil)
 }
 
 var panicdiv *gc.Node
