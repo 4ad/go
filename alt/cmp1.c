fa53602c4e4c3095b4a5d17875c6a6d115c1aef0
Aram Hăvărneanu <aram@mgk.ro>
2016-03-30 12:14:40 +0200

    alt: add C and Go comparison test
    Change-Id: I81e3c1963bc1c748663a5e07349b2b2bfa9a79ba

---
+++ alt/cmp1.c
@@ -0,0 +1,11 @@
+int
+f(void)
+{
+	int x, y;
+
+	x = 5; y = 6;
+	if (x < y) {
+		return 1;
+	}
+	return 2;
+}
---
+++ alt/cmp1.go
@@ -0,0 +1,10 @@
+package foo
+
+func f() int {
+	var x, y = 5, 6
+
+	if x < y {
+		return 1
+	}
+	return 2
+}
