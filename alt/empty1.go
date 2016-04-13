7984d5c99f70b8a7bf9c1aa9c6a9cc8621144ee8
Aram Hăvărneanu <aram@mgk.ro>
2016-04-13 19:30:38 +0200

    alt: cgo_import_dynamic test
    Change-Id: I4dd9a371ed719aac581e5f6f033aeaa794e736ca

--- alt/empty1.go
+++ alt/empty1.go
@@ -2,9 +2,18 @@ package main
 
 import "unsafe"
 
+//go:cgo_import_dynamic libc_write write "libc.so"
+//go:linkname libc_write libc_write
+var libc_write uintptr
+
+//go:nosplit
+func foo(v uintptr) uintptr {
+	return v
+}
+
 //go:linkname main main
 //go:nosplit
 func main() {
-	x := 42
-	_ = unsafe.Sizeof(x)
+	_ = unsafe.Sizeof(libc_write)
+	foo(libc_write)
 }
