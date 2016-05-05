e847bf2ce849437e4b2ed48fefd8307c6325b695
Aram Hăvărneanu <aram@mgk.ro>
2016-05-05 16:08:52 +0200

    alt: compile both optimized and non-optimized C objects, and add another leaf frame test
    Change-Id: I27cbbf34a2109f66acf7cee176fb17c61ea039b0

---
+++ alt/call2.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int __attribute__ ((noinline))
+foo3(void)
+{
+	return 3;
+}
+
+int __attribute__ ((noinline))
+foo2(int a0, int a1, int a2, int a3, int a4, int a5, int b6, int b7)
+{
+	return 2;
+}
+
+int __attribute__ ((noinline))
+foo1(int a0, int a1, int a2, int a3, int a4, int a5, int b6, int b7)
+{
+	return b6+b7;
+}
+
+int __attribute__ ((noinline))
+foo0(int a0, int a1, int a2, int a3, int a4, int a5, int b6, int b7)
+{
+	return foo1(0, 1, 2, 3, 4, 5, 6, 7) + foo2(0, 1, 2, 3, 4, 5, 6, 7) + foo3();
+}
+
+int
+main()
+{
+	return foo0(0, 1, 2, 3, 4, 5, 6, 7);
+}
