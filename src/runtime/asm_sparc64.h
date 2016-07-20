a51d3a46b7e5c5cfde90824fb033ac893c9fbdea
Shawn Walker-Salas <shawn.walker@oracle.com>
2016-07-20 11:37:02 -0700

    runtime: add explanatory text for asm register usage on sparc64
    This may need slight corrections, but is a best attempt to summarise based
    on Aram's recent explanations.

--- src/runtime/asm_sparc64.h
+++ src/runtime/asm_sparc64.h
@@ -20,8 +20,21 @@
 // So a function that sets up a stack frame at all uses as least FIXED_FRAME
 // bytes of stack. This mostly affects assembly that calls other functions
 // with arguments (the arguments should be stored at FIXED_FRAME+0(BSP),
-// FIXED_FRAME+8(BSP) etc) and some other low-level places.
-
+// FIXED_FRAME+8(BSP) etc.) and some other low-level places.
+//
+// Register usage conventions keeping the above in mind:
+//
+// * FP will make use of RFP, which is not always appropiate, e.g. in NOFRAME
+//   functions.
+// * Generally, push arguments to off(BFP); in NOFRAME functions use
+//   use off(BSP).
+// * Example: setcallerpc does not return any arguments, it modifies the
+//   link register saved on the caller's stack, so it has to use BFP.
+// * Generally, use ret+off(FP) so go vet can actually check it for
+//   corectness if there's a go declaration.  Although this cannot be used in
+//   NOFRAME functions, etc.
+//
+// See runtime/stack.go for details.
 #define ARG_PUSH_SIZE 6*8
 #define WINDOW_SIZE 16*8
 #define FIXED_FRAME WINDOW_SIZE+ARG_PUSH_SIZE
