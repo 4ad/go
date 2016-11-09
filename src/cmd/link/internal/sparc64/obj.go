19faed04c243e667e11a44669a870baaf96d0014
Shawn Walker-Salas <shawn.walker@oracle.com>
2016-11-09 11:49:02 -0800

    cmd: enable external linking by default for sparc64 on Solaris

--- src/cmd/link/internal/sparc64/obj.go
+++ src/cmd/link/internal/sparc64/obj.go
@@ -98,7 +98,7 @@ func archinit() {
 		if ld.Linkmode == ld.LinkExternal && obj.Getgoextlinkenabled() != "1" {
 			log.Fatalf("cannot use -linkmode=external with -H %s", ld.Headstr(int(ld.HEADTYPE)))
 		}
-	case obj.Hlinux, obj.Hdarwin:
+	case obj.Hlinux, obj.Hsolaris:
 		break
 	}
 
