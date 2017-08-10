4ba56276d92080f96158e3131928a4ea5639fe59
Shawn Walker-Salas <shawn.walker@oracle.com>
2017-08-10 12:30:36 -0700

    test: sliceopt.go; skip sparc64 just as amd64, etc. do

--- test/sliceopt.go
+++ test/sliceopt.go
@@ -1,4 +1,4 @@
-// +build !amd64,!arm,!amd64p32,!386,!arm64,!ppc64le,!mips64,!mips64le,!s390x
+// +build !amd64,!arm,!amd64p32,!386,!arm64,!ppc64le,!mips64,!mips64le,!s390x,!sparc64
 // errorcheck -0 -d=append,slice
 
 // Copyright 2015 The Go Authors. All rights reserved.
